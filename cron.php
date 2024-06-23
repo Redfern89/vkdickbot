db30ea584cbebfdc9b09ca90ff60b5f599ebcd72
Redfern89 <redfern.89@yandex.ru>
2024-06-23 17:02:53 +0300

    modified:   cron.php 	modified:   setup/vkbot.sql

--- cron.php
+++ cron.php
@@ -1,11 +1,13 @@
 <?php
+	if (php_sapi_name() !== 'cli') die("Ты избрал не тот путь");
+
 	define ('DOCROOT', realpath(__DIR__));
 	require_once DOCROOT . '/bootstrap.php';
-	
+
 	updateGlobal('cron_work', 'true');
 	CRON_updateDicks();
 	CRON_reloadPeers();
 	updateGlobal('cron_work', 'false');
-	
+
 	$sql -> close();
 ?>
